c85f68253494f4ba03f6512f144aa309c6e4d3b4
ChopinGuo <gshaop1@lsu.edu>
2021-04-07 22:39:58 -0500

    Generate roads.

--- ppMain.m
+++ ppMain.m
@@ -8,7 +8,7 @@ G = digraph(s,t);         % This map is a digraph.
 I = full(incidence(G)); 
 plot(G)
 
-% Generate rewards.
+%% Generate rewards.
 numInter = size(I,1);
 numEdge = size(I,2);
 v0 = converterMPH(70);                  % average velocity limitation: 70mph 
@@ -18,3 +18,11 @@ vmax = v0 * (1 + rand(numEdge,1));      % generate maximum velocity for each
 len0 = 10 * 1000;                       % average road length: 10km               
 len = len0 * (1 + rand(numEdge,1));     % generate road lenth for each
                                         % road segment.
+% Generate roads.                                        
+road12 = road(len(1),vmax(1));
+road13 = road(len(2),vmax(2));
+road24 = road(len(3),vmax(3));
+road25 = road(len(4),vmax(4));
+road35 = road(len(5),vmax(5));
+road46 = road(len(6),vmax(6));
+road56 = road(len(7),vmax(7));
